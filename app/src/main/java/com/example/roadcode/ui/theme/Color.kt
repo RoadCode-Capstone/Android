1a58f05d03bf42db58cd5ea8bea14becf97ed5b1
rkqls4764 <rkqls4764@naver.com>
2025-04-30 23:50:25 +0900

    Style: 색상 키워드 정의
    주 색상, 포인트 색, 요소 배경색의 키워드 정의

--- app/src/main/java/com/example/roadcode/ui/theme/Color.kt
+++ app/src/main/java/com/example/roadcode/ui/theme/Color.kt
@@ -9,3 +9,7 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val PrimaryColor = Color(0xFF2B3440)     // 주 색상
+val PointColor = Color(0xFFF2C53D)       // 포인트 색
+val BackGrayColor = Color(0xFFF9F9F9)    // 요소 배경색
