8dde4ffc538a04d4b76a53780658f481599ca9d3
taegyun1110 <7xorbs@gmail.com>
2025-06-18 18:25:01 +0900

    png 수정 및 Screen 추가 MyPageScreen 추가

--- app/src/main/java/com/example/roadcode/view/LoginScreen.kt
+++ app/src/main/java/com/example/roadcode/view/LoginScreen.kt
@@ -15,7 +15,7 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-
+//
 @Composable
 fun LoginScreen(modifier: Modifier = Modifier) {
     var email by remember { mutableStateOf("") }
