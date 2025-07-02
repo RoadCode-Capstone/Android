5dfb829b2c0c3bf99e616a3602fff578fb123526
rkqls4764 <rkqls4764@naver.com>
2025-07-02 16:14:40 +0900

    Feature: 공통 응답 DTO 구조 생성

---
+++ app/src/main/java/com/example/roadcode/data/model/ResponseUtilDTO.kt
@@ -0,0 +1,10 @@
+package com.example.roadcode.data.model
+
+/* 공통 DTO */
+object ResponseUtilDTO {
+    data class Response(
+        val code: String,
+        val message: String? = null,
+        val data: List<String>? = null
+    )
+}
