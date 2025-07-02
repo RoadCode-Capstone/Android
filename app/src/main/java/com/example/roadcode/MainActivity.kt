cd9cb3e43c1473f946e53512ab991d8f116f74c1
rkqls4764 <rkqls4764@naver.com>
2025-07-02 13:32:16 +0900

    Feature: Hilt 설정 추가

--- app/src/main/java/com/example/roadcode/MainActivity.kt
+++ app/src/main/java/com/example/roadcode/MainActivity.kt
@@ -1,46 +1,23 @@
 package com.example.roadcode
 
+import android.app.Application
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.roadcode.ui.theme.RoadCodeTheme
+import com.example.roadcode.navitagion.MainNavGraph
+import com.example.roadcode.view.*
+import dagger.hilt.android.AndroidEntryPoint
+import dagger.hilt.android.HiltAndroidApp
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            RoadCodeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+            MainNavGraph()
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    RoadCodeTheme {
-        Greeting("Android")
-    }
-}
+@HiltAndroidApp
+class RoadCodeApplication : Application()
