e821be46dc1fa801e7169e0daeb0582d2dbcab2e
rkqls4764 <rkqls4764@naver.com>
2025-07-07 14:37:41 +0900

    Feature: 학습 계획 관리 viewmodel 생성 및 구현

--- app/src/main/java/com/example/roadcode/viewmodel/RoadmapPlanViewModel.kt
+++ app/src/main/java/com/example/roadcode/viewmodel/RoadmapPlanViewModel.kt
@@ -9,7 +9,7 @@ import javax.inject.Inject
 
 @HiltViewModel
 class RoadmapPlanViewModel @Inject constructor() : ViewModel() {
-    // 학습 계획 데이터 클래스
+    /* 학습 계획 데이터 클래스 */
     data class Plan(
         val selectedLanguage: String? = null,
         val selectedType: String? = null,
@@ -20,19 +20,23 @@ class RoadmapPlanViewModel @Inject constructor() : ViewModel() {
     private val _plan = MutableStateFlow(Plan())
     val plan = _plan.asStateFlow()
 
+    /* 사용 언어 설정 함수 */
     fun setSelectedLanguage(language: String?) {
         _plan.value = _plan.value.copy(selectedLanguage = language)
         Log.d("언어", "언어 수정 ${_plan.value.selectedLanguage}")
     }
 
+    /* 로드맵 유형 설정 함수 */
     fun setSelectedType(type: String?) {
         _plan.value = _plan.value.copy(selectedType = type)
     }
 
+    /* 학습할 알고리즘 설정 함수 */
     fun setSelectedAlgorithm(algorithm: String?) {
         _plan.value = _plan.value.copy(selectedAlgorithm = algorithm)
     }
 
+    /* 일일 학습 목표 설정 함수 */
     fun setSelectedGoal(goal: Int?) {
         _plan.value = _plan.value.copy(selectedGoal = goal)
     }
