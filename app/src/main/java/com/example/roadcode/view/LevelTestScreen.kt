832f92bb3f67f8b9d41969f77ac971409c7d8eb4
rkqls4764 <rkqls4764@naver.com>
2025-07-07 16:03:17 +0900

    Feature: 레벨 테스트 준비 화면 구현

---
+++ app/src/main/java/com/example/roadcode/view/LevelTestScreen.kt
@@ -0,0 +1,195 @@
+package com.example.roadcode.view
+
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavController
+import com.example.roadcode.R
+import com.example.roadcode.ui.theme.BackGrayColor
+import com.example.roadcode.ui.theme.PointColor
+import com.example.roadcode.ui.theme.PrimaryColor
+import com.example.roadcode.viewmodel.RoadmapPlanViewModel
+
+/* 레벨 테스트 준비 화면 */
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun LevelTestReadyScreen(navController: NavController, roadmapViewModel: RoadmapPlanViewModel) {
+    val plan by roadmapViewModel.plan.collectAsState()
+
+    val infos = listOf(
+        "제한 시간은 2시간이에요.",
+        "사용 언어는 '${plan.selectedLanguage}'에요.",
+        "테스트 내용은 '${if (plan.selectedAlgorithm == null) "언어" else plan.selectedAlgorithm}'에요.",
+        "제한 시간이 종료되거나, 종료 버튼을 누르면 종료돼요."
+    )
+
+    Scaffold(
+        topBar = {
+            CenterAlignedTopAppBar(
+                title = {
+                    Text(
+                        text = "레벨 테스트",
+                        fontSize = 18.sp,
+                        color = PrimaryColor,
+                        fontFamily = FontFamily(Font(R.font.spoqahansansneo_medium))
+                    )
+                },
+                navigationIcon = {
+                    IconButton(
+                        onClick = { navController.popBackStack() }
+                    ) {
+                        Icon(
+                            imageVector = Icons.Default.ArrowBack,
+                            contentDescription = "뒤로 가기 버튼",
+                            tint = PrimaryColor
+                        )
+                    }
+                }
+            )
+        }
+    ) { paddingValues ->
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(paddingValues)
+        ) {
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(bottom = 160.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Spacer(modifier = Modifier.height(36.dp))
+
+                Text(
+                    text = "레벨 테스트를 시작할게요",
+                    fontSize = 16.sp,
+                    fontFamily = FontFamily(Font(R.font.spoqahansansneo_light))
+                )
+
+                Spacer(modifier = Modifier.height(50.dp))
+
+                // 주의사항 출력
+                Column(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 30.dp)
+                        .background(color = BackGrayColor)
+                        .border(width = 0.5.dp, color = Color.Black),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Spacer(modifier = Modifier.height(30.dp))
+                    
+                    Text(
+                        text = "주의사항",
+                        fontSize = 17.sp,
+                        fontFamily = FontFamily(Font(R.font.spoqahansansneo_medium))
+                    )
+
+                    Spacer(modifier = Modifier.height(25.dp))
+
+                    Column(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .wrapContentHeight()    // 높이를 내용만큼만
+                            .padding(horizontal = 30.dp)
+                    ) {
+                        infos.forEach { info ->
+                            Row(modifier = Modifier.fillMaxWidth()) {
+                                Text(text = "•", modifier = Modifier.padding(end = 8.dp))
+                                Text(
+                                    text = info,
+                                    fontSize = 16.sp,
+                                    fontFamily = FontFamily(Font(R.font.spoqahansansneo_light))
+                                )
+                            }
+
+                            Spacer(modifier = Modifier.height(5.dp))
+                        }
+                    }
+
+                    Spacer(modifier = Modifier.height(30.dp))
+                }
+            }
+
+            Row(
+                modifier = Modifier
+                    .align(Alignment.BottomCenter)
+                    .padding(start = 30.dp, end = 30.dp, bottom = 40.dp)
+            ) {
+                Button( // 시작하기 버튼
+                    onClick = {
+                        navController.navigate("level_test")
+                    },
+                    modifier = Modifier
+                        .weight(1f)
+                        .height(50.dp),
+                    shape = RoundedCornerShape(20.dp),
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = PointColor,
+                        contentColor = Color.White
+                    )
+                ) {
+                    Text(
+                        text = "시작하기",
+                        fontSize = 16.sp,
+                        color = Color.White,
+                        fontFamily = FontFamily(Font(R.font.spoqahansansneo_medium))
+                    )
+                }
+            }
+        }
+    }
+}
+
+/* 레벨 테스트 화면 */
+@Composable
+fun LevelTestScreen(navController: NavController) {
+
+}
+
+/* 레벨 테스트 결과 화면 */
+@Composable
+fun LevelTestResultScreen(navController: NavController) {
+
+}
