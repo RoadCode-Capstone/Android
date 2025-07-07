7ac0880df137ecf0e0c94678aaafa907ca608342
rkqls4764 <rkqls4764@naver.com>
2025-07-07 14:00:36 +0900

    Feature: 공통 응답 DTO 수정에 맞춰 변경

---
+++ app/src/main/java/com/example/roadcode/data/repository/TagRepository.kt
@@ -0,0 +1,35 @@
+package com.example.roadcode.data.repository
+
+import android.util.Log
+import com.example.roadcode.data.model.ResponseUtilDTO
+import com.example.roadcode.retrofit.JsonService
+import com.example.roadcode.retrofit.RetrofitInstance
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import retrofit2.HttpException
+import retrofit2.create
+import javax.inject.Inject
+
+class TagRepository @Inject constructor() {
+    private val jsonService: JsonService = RetrofitInstance.retrofit.create(JsonService::class.java)
+
+    /* 태그 목록 조회 */
+    suspend fun fetchTags(): Flow<Result<List<String>>> = flow {
+        try {
+            val response = jsonService.getTags("Bearer fixed-test-token")
+            if (response.isSuccessful) {
+                val tags = response.body()?.data?.get("names") as? List<String>
+
+                if (tags != null) {
+                    emit(Result.success(tags))
+                } else {
+                    emit(Result.failure(NullPointerException("data가 null입니다.")))
+                }
+            } else {
+                throw HttpException(response)
+            }
+        } catch (e: Exception) {
+            emit(Result.failure(e))
+        }
+    }
+}
