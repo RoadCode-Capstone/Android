0b37202ff843db0a615f74203f42929a09297afb
rkqls4764 <rkqls4764@naver.com>
2025-07-08 11:51:03 +0900

    Feature: 레벨 테스트 화면 구현

--- app/src/main/java/com/example/roadcode/view/LevelTestScreen.kt
+++ app/src/main/java/com/example/roadcode/view/LevelTestScreen.kt
@@ -28,7 +28,9 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -39,20 +41,24 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import com.example.roadcode.R
+import com.example.roadcode.data.model.LevelTestDTO
 import com.example.roadcode.ui.theme.BackGrayColor
 import com.example.roadcode.ui.theme.PointColor
 import com.example.roadcode.ui.theme.PrimaryColor
+import com.example.roadcode.viewmodel.LevelTestViewModel
 import com.example.roadcode.viewmodel.RoadmapPlanViewModel
+import kotlinx.coroutines.delay
 
 /* 레벨 테스트 준비 화면 */
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun LevelTestReadyScreen(navController: NavController, roadmapViewModel: RoadmapPlanViewModel) {
+fun LevelTestReadyScreen(navController: NavController, roadmapViewModel: RoadmapPlanViewModel, levelTestViewModel: LevelTestViewModel) {
     val plan by roadmapViewModel.plan.collectAsState()
 
     val infos = listOf(
@@ -159,6 +165,9 @@ fun LevelTestReadyScreen(navController: NavController, roadmapViewModel: Roadmap
             ) {
                 Button( // 시작하기 버튼
                     onClick = {
+                        // 레벨 테스트 생성
+                        val request = LevelTestDTO.createRequest(plan.selectedType!!, plan.selectedLanguage!!, if (plan.selectedType == "언어") null else plan.selectedAlgorithm)
+//                        levelTestViewModel.createLevelTest(request)
                         navController.navigate("level_test")
                     },
                     modifier = Modifier
@@ -183,9 +192,116 @@ fun LevelTestReadyScreen(navController: NavController, roadmapViewModel: Roadmap
 }
 
 /* 레벨 테스트 화면 */
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun LevelTestScreen(navController: NavController) {
+fun LevelTestScreen(navController: NavController, levelTestViewModel: LevelTestViewModel) {
+    var remainingSeconds by remember { mutableStateOf(120 * 60) }   // 남은 시간 (120분부터 시작)
 
+    LaunchedEffect(Unit) {
+        // 남은 시간 감소 (0 되면 풀이 제출)
+        while (remainingSeconds > 0) {
+            delay(1000)
+            remainingSeconds -= 1
+        }
+    }
+
+    val levelTestIds by levelTestViewModel.levelTestIds.collectAsState()
+    val formattedTime = String.format("%d:%02d", remainingSeconds / 60, remainingSeconds % 60)
+
+    Scaffold(
+        topBar = {
+            CenterAlignedTopAppBar(
+                title = {
+                    Text(
+                        text = "레벨 테스트",
+                        fontSize = 18.sp,
+                        color = PrimaryColor,
+                        fontFamily = FontFamily(Font(R.font.spoqahansansneo_medium))
+                    )
+                },
+                navigationIcon = {
+                    Button( // 종료 버튼
+                        onClick = {
+                                  /* 풀이 제출 */
+                        },
+                        modifier = Modifier.height(40.dp).padding(start = 15.dp),
+                        shape = RoundedCornerShape(20.dp),
+                        colors = ButtonDefaults.buttonColors(
+                            containerColor = PrimaryColor,
+                            contentColor = Color.White
+                        )
+                    ) {
+                        Text(
+                            text = "종료",
+                            fontSize = 16.sp,
+                            color = Color.White,
+                            fontFamily = FontFamily(Font(R.font.spoqahansansneo_medium))
+                        )
+                    }
+                },
+                actions = {
+                    Text( // 남은 시간 출력
+                        text = formattedTime,
+                        fontSize = 16.sp,
+                        color = PrimaryColor,
+                        fontFamily = FontFamily(Font(R.font.spoqahansansneo_medium)),
+                        modifier = Modifier.padding(end = 20.dp)
+                    )
+                }
+            )
+        }
+    ) { paddingValues ->
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(paddingValues)
+        ) {
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(bottom = 160.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Spacer(modifier = Modifier.height(36.dp))
+
+                // 문제 출력
+            }
+/*
+            Row(
+                modifier = Modifier
+                    .align(Alignment.BottomCenter)
+                    .padding(start = 30.dp, end = 30.dp, bottom = 40.dp)
+            ) {
+                Button( // 시작하기 버튼
+                    onClick = {
+                        // 레벨 테스트 생성
+                        val request = LevelTestDTO.createRequest(
+                            plan.selectedType!!,
+                            plan.selectedLanguage!!,
+                            if (plan.selectedType == "언어") null else plan.selectedAlgorithm
+                        )
+                        levelTestViewModel.createLevelTest(request)
+                        navController.navigate("level_test")
+                    },
+                    modifier = Modifier
+                        .weight(1f)
+                        .height(50.dp),
+                    shape = RoundedCornerShape(20.dp),
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = PointColor,
+                        contentColor = Color.White
+                    )
+                ) {
+                    Text(
+                        text = "시작하기",
+                        fontSize = 16.sp,
+                        color = Color.White,
+                        fontFamily = FontFamily(Font(R.font.spoqahansansneo_medium))
+                    )
+                }
+            }*/
+        }
+    }
 }
 
 /* 레벨 테스트 결과 화면 */
