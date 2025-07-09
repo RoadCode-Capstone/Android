feee3337e8f5b104703f59d2bdce021f28d3f626
rkqls4764 <rkqls4764@naver.com>
2025-07-09 16:24:33 +0900

    Feature: Divider 색상 추가

--- app/src/main/java/com/example/roadcode/ui/theme/Color.kt
+++ app/src/main/java/com/example/roadcode/ui/theme/Color.kt
@@ -10,6 +10,7 @@ val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
 
-val PrimaryColor = Color(0xFF2B3440)     // 주 색상
-val PointColor = Color(0xFFF2C53D)       // 포인트 색
-val BackGrayColor = Color(0xFFF9F9F9)    // 요소 배경색
+val PrimaryColor = Color(0xFF2B3440)    // 주 색상
+val PointColor = Color(0xFFF2C53D)      // 포인트 색
+val BackGrayColor = Color(0xFFF9F9F9)   // 요소 배경색
+val LineColor = Color(0xFFB3B3B3)       // 선 색
