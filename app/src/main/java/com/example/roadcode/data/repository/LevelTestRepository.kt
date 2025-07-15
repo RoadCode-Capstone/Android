ffb849d64044b629026be9a54a37da5911f72d13
rkqls4764 <rkqls4764@naver.com>
2025-07-15 22:19:09 +0900

    Feature: 레벨 테스트 관련 통신 기능 구현

--- app/src/main/java/com/example/roadcode/data/repository/LevelTestRepository.kt
+++ app/src/main/java/com/example/roadcode/data/repository/LevelTestRepository.kt
@@ -1,6 +1,8 @@
 package com.example.roadcode.data.repository
 
 import com.example.roadcode.data.model.LevelTestDTO
+import com.example.roadcode.data.model.ProblemDTO
+import com.example.roadcode.data.model.SubmissionDTO
 import com.example.roadcode.retrofit.JsonService
 import com.example.roadcode.retrofit.RetrofitInstance
 import kotlinx.coroutines.flow.Flow
@@ -10,16 +12,62 @@ import javax.inject.Inject
 
 class LevelTestRepository @Inject constructor() {
     private val jsonService: JsonService = RetrofitInstance.retrofit.create(JsonService::class.java)
+    private val token: String = "Bearer fixed-test-token"
 
     /* 레벨 테스트 생성 */
     suspend fun createLevelTest(request: LevelTestDTO.createRequest): Flow<Result<List<Long>>> = flow {
         try {
-            val response = jsonService.createLevelTest("Bearer fixed-test-token", request)
+            val response = jsonService.createLevelTest(token, request)
             if (response.isSuccessful) {
-                val levelTestIds = response.body()?.data?.get("problemIds") as List<Long>
+                val body = response.body()
+                if (body?.code != "SUCCESS") {
+                    emit(Result.failure(Exception("${body?.code.toString()}: ${body?.message.toString()}")))
+                }
+
+                val levelTestIds = body!!.data!!.problemIds
                 emit(Result.success(levelTestIds))
             } else {
-                throw HttpException(response)
+                emit(Result.failure(HttpException(response)))
+            }
+        } catch (e: Exception) {
+            emit(Result.failure(e))
+        }
+    }
+
+    /* 레벨 테스트 문제 조회 */
+    suspend fun getLevelTestProblems(request: List<Long>): Flow<Result<List<ProblemDTO.ProblemData>>> = flow {
+        try {
+            val response = jsonService.getLevelTestProblems(token, request)
+            if (response.isSuccessful) {
+                val body = response.body()
+                if (body?.code != "SUCCESS") {
+                    emit(Result.failure(Exception("${body?.code.toString()}: ${body?.message.toString()}")))
+                }
+
+                val levelTestProblems = body!!.data!!.problems
+                emit(Result.success(levelTestProblems))
+            } else {
+                emit(Result.failure(HttpException(response)))
+            }
+        } catch (e: Exception) {
+            emit(Result.failure(e))
+        }
+    }
+
+    /* 레벨 테스트 제출 */
+    suspend fun submitLevelTest(request: LevelTestDTO.submitRequest): Flow<Result<LevelTestDTO.submitResponse>> = flow {
+        try {
+            val response = jsonService.submitLevelTest(token, request)
+            if (response.isSuccessful) {
+                val body = response.body()
+                if (body?.code != "SUCCESS") {
+                    emit(Result.failure(Exception("${body?.code.toString()}: ${body?.message.toString()}")))
+                }
+
+                val levelTestIds = body!!.data!!
+                emit(Result.success(levelTestIds))
+            } else {
+                emit(Result.failure(HttpException(response)))
             }
         } catch (e: Exception) {
             emit(Result.failure(e))
