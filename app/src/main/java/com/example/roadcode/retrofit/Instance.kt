0c2cc247a25e25c40d362f08d74b78c2c229899e
rkqls4764 <rkqls4764@naver.com>
2025-07-15 22:18:41 +0900

    Feature: 서버 응답 대기 시간 늘림 (레벨 테스트 제출 기능에서 필요)

--- app/src/main/java/com/example/roadcode/retrofit/Instance.kt
+++ app/src/main/java/com/example/roadcode/retrofit/Instance.kt
@@ -2,9 +2,11 @@ package com.example.roadcode.retrofit
 
 import com.google.gson.Gson
 import com.google.gson.GsonBuilder
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.converter.scalars.ScalarsConverterFactory
+import java.util.concurrent.TimeUnit
 
 object RetrofitInstance {
     private const val BASE_URL = "http://3.35.192.94:8080" // BASE URL 설정
@@ -13,11 +15,19 @@ object RetrofitInstance {
         .setLenient()
         .create()
 
+    // timeout 설정 추가
+    private val okHttpClient: OkHttpClient = OkHttpClient.Builder()
+        .connectTimeout(30, TimeUnit.SECONDS) // 연결 시도 제한 시간
+        .readTimeout(30, TimeUnit.SECONDS)    // 서버 응답 대기 시간
+        .writeTimeout(30, TimeUnit.SECONDS)   // 서버로 데이터 쓰는 시간
+        .build()
+
     val retrofit: Retrofit by lazy {
         Retrofit.Builder()
             .baseUrl(BASE_URL)
-            .addConverterFactory(ScalarsConverterFactory.create()) //텍스트 데이터 처리
-            .addConverterFactory(GsonConverterFactory.create(gson)) //Json 데이터 처리
+            .client(okHttpClient)
+            .addConverterFactory(ScalarsConverterFactory.create())
+            .addConverterFactory(GsonConverterFactory.create(gson))
             .build()
     }
 
