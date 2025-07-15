de8a02a707c94604d0f778866e3302ac73d2d263
rkqls4764 <rkqls4764@naver.com>
2025-07-15 22:16:56 +0900

    Feature: 레벨 테스트 생성, 문제 조회, 제출 기능 구현

--- app/src/main/java/com/example/roadcode/retrofit/Service.kt
+++ app/src/main/java/com/example/roadcode/retrofit/Service.kt
@@ -2,6 +2,7 @@ package com.example.roadcode.retrofit
 
 import com.example.roadcode.data.model.LevelTestDTO
 import com.example.roadcode.data.model.ResponseUtilDTO
+import com.example.roadcode.data.model.TagDTO
 import retrofit2.Call
 import retrofit2.Response
 import retrofit2.http.Body
@@ -9,14 +10,23 @@ import retrofit2.http.GET
 import retrofit2.http.Header
 import retrofit2.http.Headers
 import retrofit2.http.POST
+import retrofit2.http.Query
 
 /* JSON 데이터 처리 */
 interface JsonService {
     // 태그 목록 조회
     @GET("/api/v1/tags")
-    suspend fun getTags(@Header("Authorization") token: String): Response<ResponseUtilDTO.Response>
+    suspend fun getTags(): Response<ResponseUtilDTO.Response<TagDTO.TagsResponse>>
 
     // 레벨 테스트 생성
     @POST("/api/v1/level-test")
-    suspend fun createLevelTest(@Header("Authorization") token: String, @Body request: LevelTestDTO.createRequest): Response<ResponseUtilDTO.Response>
+    suspend fun createLevelTest(@Header("Authorization") token: String, @Body request: LevelTestDTO.createRequest): Response<ResponseUtilDTO.Response<LevelTestDTO.createResponse>>
+
+    // 레벨 테스트 문제 조회
+    @GET("/api/v1/problems")
+    suspend fun getLevelTestProblems(@Header("Authorization") token: String, @Query("ids") ids: List<Long>): Response<ResponseUtilDTO.Response<LevelTestDTO.getResponse>>
+
+    // 레벨 테스트 제출
+    @POST("/api/v1/level-test/submissions")
+    suspend fun submitLevelTest(@Header("Authorization") token: String, @Body request: LevelTestDTO.submitRequest): Response<ResponseUtilDTO.Response<LevelTestDTO.submitResponse>>
 }
