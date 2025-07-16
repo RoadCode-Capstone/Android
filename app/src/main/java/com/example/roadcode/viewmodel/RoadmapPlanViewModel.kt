854c4e5307828428211c84a49b72c277d496fdb1
rkqls4764 <rkqls4764@naver.com>
2025-07-16 11:24:31 +0900

    Feature: 로그 추가

--- app/src/main/java/com/example/roadcode/viewmodel/RoadmapPlanViewModel.kt
+++ app/src/main/java/com/example/roadcode/viewmodel/RoadmapPlanViewModel.kt
@@ -9,6 +9,10 @@ import javax.inject.Inject
 
 @HiltViewModel
 class RoadmapPlanViewModel @Inject constructor() : ViewModel() {
+    companion object {
+        private const val TAG = "RoadmapPlanViewModel"
+    }
+
     /* 학습 계획 데이터 클래스 */
     data class Plan(
         val selectedLanguage: String? = null,
@@ -23,21 +27,24 @@ class RoadmapPlanViewModel @Inject constructor() : ViewModel() {
     /* 사용 언어 설정 함수 */
     fun setSelectedLanguage(language: String?) {
         _plan.value = _plan.value.copy(selectedLanguage = language)
-        Log.d("언어", "언어 수정 ${_plan.value.selectedLanguage}")
+        Log.d(TAG, "학습 계획 - 언어 변경: ${_plan.value.selectedLanguage}")
     }
 
     /* 로드맵 유형 설정 함수 */
     fun setSelectedType(type: String?) {
         _plan.value = _plan.value.copy(selectedType = type)
+        Log.d(TAG, "학습 계획 - 유형 변경: ${_plan.value.selectedType}")
     }
 
     /* 학습할 알고리즘 설정 함수 */
     fun setSelectedAlgorithm(algorithm: String?) {
         _plan.value = _plan.value.copy(selectedAlgorithm = algorithm)
+        Log.d(TAG, "학습 계획 - 알고리즘 변경: ${_plan.value.selectedAlgorithm}")
     }
 
     /* 일일 학습 목표 설정 함수 */
     fun setSelectedGoal(goal: Int?) {
         _plan.value = _plan.value.copy(selectedGoal = goal)
+        Log.d(TAG, "학습 계획 - 일일 학습 목표 변경: ${_plan.value.selectedGoal}")
     }
 }
