380a52591dc3c7256505b916268f9d39206eb976
rkqls4764 <rkqls4764@naver.com>
2025-07-16 11:25:42 +0900

    Feature: 이동할 화면 구성

--- app/src/main/java/com/example/roadcode/MainActivity.kt
+++ app/src/main/java/com/example/roadcode/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.example.roadcode
 
 import android.app.Application
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.annotation.RequiresApi
 import com.example.roadcode.navitagion.MainNavGraph
 import com.example.roadcode.view.*
 import dagger.hilt.android.AndroidEntryPoint
@@ -11,6 +13,7 @@ import dagger.hilt.android.HiltAndroidApp
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
--- app/src/main/java/com/example/roadcode/navitagion/MainNavGraph.kt
+++ app/src/main/java/com/example/roadcode/navitagion/MainNavGraph.kt
@@ -1,5 +1,7 @@
 package com.example.roadcode.navitagion
 
+import android.os.Build
+import androidx.annotation.RequiresApi
 import androidx.compose.runtime.Composable
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
@@ -16,18 +18,19 @@ import com.example.roadcode.view.RoadmapPlanTypeScreen
 import com.example.roadcode.viewmodel.LevelTestViewModel
 import com.example.roadcode.viewmodel.RoadmapPlanViewModel
 
+@RequiresApi(Build.VERSION_CODES.O)
 @Composable
 fun MainNavGraph(navController: NavHostController = rememberNavController()) {
     val roadmapPlanViewModel: RoadmapPlanViewModel = hiltViewModel()
     val levelTestViewModel: LevelTestViewModel = hiltViewModel()
 
     NavHost(navController = navController, startDestination = "plan_language") {
-        composable("plan_language") { RoadmapPlanLanguageScreen(navController, roadmapPlanViewModel) }
-        composable("plan_type") { RoadmapPlanTypeScreen(navController, roadmapPlanViewModel) }
-        composable("plan_algorithm") { RoadmapPlanAlgorithmScreen(navController, roadmapPlanViewModel) }
-        composable("plan_goal") { RoadmapPlanGoalScreen(navController, roadmapPlanViewModel) }
-        composable("level_ready") { LevelTestReadyScreen(navController, roadmapPlanViewModel, levelTestViewModel) }
-        composable("level_test") { LevelTestScreen(navController, levelTestViewModel) }
-        composable("level_result") { LevelTestResultScreen(navController) }
+        composable("plan_language") { RoadmapPlanLanguageScreen(navController, roadmapPlanViewModel) }              // 학습 계획 설정 화면 (언어)
+        composable("plan_type") { RoadmapPlanTypeScreen(navController, roadmapPlanViewModel) }                      // 학습 계획 설정 화면 (유형)
+        composable("plan_algorithm") { RoadmapPlanAlgorithmScreen(navController, roadmapPlanViewModel) }            // 학습 계획 설정 화면 (알고리즘)
+        composable("plan_goal") { RoadmapPlanGoalScreen(navController, roadmapPlanViewModel) }                      // 학습 계획 설정 화면 (일일 학습 목표)
+        composable("level_ready") { LevelTestReadyScreen(navController, roadmapPlanViewModel, levelTestViewModel) } // 레벨 테스트 준비 화면
+        composable("level_test") { LevelTestScreen(navController, roadmapPlanViewModel, levelTestViewModel) }       // 레벨 테스트 화면
+        composable("level_result") { LevelTestResultScreen(navController, levelTestViewModel) }                     // 레벨 테스트 결과 화면
     }
 }
