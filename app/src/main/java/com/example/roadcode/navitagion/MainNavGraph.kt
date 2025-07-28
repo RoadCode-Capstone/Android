a67f0f67cdc79c289e46f91ffd2e5e08a02aa87c
rkqls4764 <rkqls4764@naver.com>
2025-07-28 21:14:24 +0900

    feat: 이동 화면 내역에 로드맵 목록 조회 화면 추가

--- app/src/main/java/com/example/roadcode/navitagion/MainNavGraph.kt
+++ app/src/main/java/com/example/roadcode/navitagion/MainNavGraph.kt
@@ -11,6 +11,7 @@ import androidx.navigation.compose.rememberNavController
 import com.example.roadcode.view.LevelTestReadyScreen
 import com.example.roadcode.view.LevelTestResultScreen
 import com.example.roadcode.view.LevelTestScreen
+import com.example.roadcode.view.RoadmapListScreen
 import com.example.roadcode.view.RoadmapPlanAlgorithmScreen
 import com.example.roadcode.view.RoadmapPlanGoalScreen
 import com.example.roadcode.view.RoadmapPlanLanguageScreen
@@ -27,7 +28,7 @@ fun MainNavGraph(navController: NavHostController = rememberNavController()) {
     val levelTestViewModel: LevelTestViewModel = hiltViewModel()
     val roadmapViewModel: RoadmapViewModel = hiltViewModel()
 
-    NavHost(navController = navController, startDestination = "plan_language") {
+    NavHost(navController = navController, startDestination = "roadmap_list") {
         composable("plan_language") { RoadmapPlanLanguageScreen(navController, roadmapPlanViewModel) }                                  // 학습 계획 설정 화면 (언어)
         composable("plan_type") { RoadmapPlanTypeScreen(navController, roadmapPlanViewModel) }                                          // 학습 계획 설정 화면 (유형)
         composable("plan_algorithm") { RoadmapPlanAlgorithmScreen(navController, roadmapPlanViewModel) }                                // 학습 계획 설정 화면 (알고리즘)
@@ -36,5 +37,6 @@ fun MainNavGraph(navController: NavHostController = rememberNavController()) {
         composable("level_test") { LevelTestScreen(navController, roadmapPlanViewModel, levelTestViewModel) }                           // 레벨 테스트 화면
         composable("level_result") { LevelTestResultScreen(navController, roadmapPlanViewModel, levelTestViewModel, roadmapViewModel) } // 레벨 테스트 결과 화면
         composable("roadmap") { RoadmapScreen(navController, roadmapViewModel) }                                                        // 로드맵 조회 화면
+        composable("roadmap_list") { RoadmapListScreen(navController, roadmapViewModel) }                                               // 로드맵 목록 화면
     }
 }
