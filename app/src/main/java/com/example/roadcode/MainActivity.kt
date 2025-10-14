f1a1c7b176e6f33d77d0b2a9f14389f8cb7c98dc
rkqls4764 <rkqls4764@naver.com>
2025-10-14 15:49:30 +0900

    feat: 알림 기능 구현(알림 클릭 시 앱 실행)

--- app/src/main/java/com/example/roadcode/MainActivity.kt
+++ app/src/main/java/com/example/roadcode/MainActivity.kt
@@ -1,12 +1,37 @@
 package com.example.roadcode
 
+import android.Manifest
 import android.app.Application
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.content.pm.PackageManager
 import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.annotation.RequiresApi
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.core.app.ActivityCompat
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import androidx.core.content.ContextCompat
 import com.example.roadcode.navitagion.MainNavGraph
+import com.example.roadcode.util.showNotification
 import com.example.roadcode.view.*
 import dagger.hilt.android.AndroidEntryPoint
 import dagger.hilt.android.HiltAndroidApp
@@ -18,6 +43,39 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MainNavGraph()
+            RequestNotificationPermission()
+        }
+    }
+
+    @Composable
+    fun RequestNotificationPermission() {
+        val permissionState = remember { mutableStateOf(checkNotificationPermission()) }
+
+        if (permissionState.value) {
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ) {
+                Text("알림 권한 받음")
+
+                Button(onClick = {
+                    showNotification(context = this@MainActivity)
+                }) {
+                    Text("알림 생성")
+                }
+            }
+        } else {
+            Text("알림 권한 필요함")
+        }
+    }
+
+    // 알림 권한 확인
+    private fun checkNotificationPermission(): Boolean {
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            ContextCompat.checkSelfPermission(this, Manifest.permission.POST_NOTIFICATIONS) == PackageManager.PERMISSION_GRANTED
+        } else {
+            true
         }
     }
 }
---
+++ app/src/main/java/com/example/roadcode/util/PushAlarm.kt
@@ -0,0 +1,63 @@
+package com.example.roadcode.util
+
+import android.Manifest
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
+import androidx.core.app.ActivityCompat
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import com.example.roadcode.MainActivity
+import com.example.roadcode.R
+
+fun showNotification(context: Context) {
+    // 알림 채널 설정 (Android 8.0 이상)
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+        val channelId = "your_channel_id"
+        val channelName = "Your Channel Name"
+        val importance = NotificationManager.IMPORTANCE_DEFAULT
+        val channel = NotificationChannel(channelId, channelName, importance).apply {
+            description = "Your Channel Description"
+        }
+        val notificationManager: NotificationManager = context.getSystemService(NotificationManager::class.java)
+        notificationManager.createNotificationChannel(channel)
+    }
+
+    val intent = Intent(context, MainActivity::class.java).apply {
+        flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+    }
+
+    val pendingIntent = PendingIntent.getActivity(
+        context,
+        0,
+        intent,
+        PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
+    )
+
+    // 알림 생성
+    val builder = NotificationCompat.Builder(context, "your_channel_id")
+        .setSmallIcon(R.drawable.mascot)
+        .setContentTitle("오늘 출석하지 않았어요")
+        .setContentText("앱에 접속해서 포인트를 받아보세요 \uD83C\uDF81")
+        .setContentIntent(pendingIntent)   // 알림 눌렀을 때 MainActivity 실행
+        .setAutoCancel(true)               // 알림 터치 시 자동 삭제
+        .setPriority(NotificationCompat.PRIORITY_HIGH)
+
+    // 알림 표시
+    with(NotificationManagerCompat.from(context)) {
+        if (ActivityCompat.checkSelfPermission(
+                context,
+                Manifest.permission.POST_NOTIFICATIONS
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            // 🔸 권한이 없으면 알림을 표시하지 않음
+            return
+        }
+
+        notify(1, builder.build())
+    }
+}
