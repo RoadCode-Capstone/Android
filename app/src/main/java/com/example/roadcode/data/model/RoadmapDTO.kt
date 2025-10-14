7e65883ab3e4ea1c19f7c640c439a54ec4feb6fc
rkqls4764 <rkqls4764@naver.com>
2025-10-14 14:05:37 +0900

    feat: 풀이 제출 기능 구현

--- app/src/main/java/com/example/roadcode/data/model/RoadmapDTO.kt
+++ app/src/main/java/com/example/roadcode/data/model/RoadmapDTO.kt
@@ -2,27 +2,27 @@ package com.example.roadcode.data.model
 
 object RoadmapDTO {
     // 로드맵 데이터 구조
-    data class roadmapData(
-        val roadmapId: Long,                // 로드맵 ID
-        val title: String,                  // 제목
-        val type: String,                   // 유형
-        val language: String,               // 언어
-        val algorithm: String,              // 알고리즘
-        val currentProblem: roadmapProblem, // 해당 로드맵에서 현재 진행 중인 문제 정보
-        val levelTestResult: Int,           // 레벨 테스트 결과
-        val dailyGoal: Int                  // 일일 학습 목표
+    data class RoadmapData(
+        val roadmapId: Long = 0,                                // 로드맵 ID
+        val title: String = "",                                 // 제목
+        val type: String = "",                                  // 유형
+        val language: String = "",                              // 언어
+        val algorithm: String = "",                             // 알고리즘
+        val currentProblem: RoadmapProblem = RoadmapProblem(),  // 해당 로드맵에서 현재 진행 중인 문제 정보
+        val levelTestResult: Int = 0,                           // 레벨 테스트 결과
+        val dailyGoal: Int = 0                                  // 일일 학습 목표
     )
 
     // 해당 로드맵에서 현재 진행 중인 문제 데이터 구조
-    data class roadmapProblem(
-        val roadmapProblemId: Long, // 로드맵 내 문제 ID
-        val problemId: Long,        // 문제 ID
-        val order: Int,             // 로드맵 내 문제 순서 (0부터 시작)
-        val status: String          // 문제 풀이 상태 (시작 전: NOT_STARTED, 진행 중: IN_PROGRESS, 완료: COMPLETED)
+    data class RoadmapProblem(
+        val roadmapProblemId: Long = 0, // 로드맵 내 문제 ID
+        val problemId: Long = 0,        // 문제 ID
+        val order: Int = 0,             // 로드맵 내 문제 순서 (0부터 시작)
+        val status: String = ""         // 문제 풀이 상태 (시작 전: NOT_STARTED, 진행 중: IN_PROGRESS, 완료: COMPLETED)
     )
 
     // 로드맵 생성 요청
-    data class createRequest(
+    data class CreateRequest(
         val type: String,           // 유형
         val language: String,       // 언어
         val algorithm: String?,     // 알고리즘
@@ -31,22 +31,22 @@ object RoadmapDTO {
     )
 
     // 로드맵 생성 응답
-    data class createResponse(
+    data class CreateResponse(
         val id: Long    // 생성된 로드맵 ID
     )
 
     // 로드맵 문제 목록 조회 응답
-    data class getProblemsResponse(
-        val roadmapProblems: List<roadmapProblem>
+    data class GetProblemsResponse(
+        val roadmapProblems: List<RoadmapProblem>
     )
 
     // 회원 로드맵 목록 조회 응답
-    data class getRoadmapsResponse(
-        val roadmaps: List<roadmapsData> // 로드맵 목록 리스트
+    data class GetRoadmapsResponse(
+        val roadmaps: List<RoadmapsData> // 로드맵 목록 리스트
     )
 
     // 로드맵 목록 데이터 구조
-    data class roadmapsData(
+    data class RoadmapsData(
         val roadmapId: Long,    // 로드맵 ID
         val title: String,      // 제목
         val type: String,       // 유형
--- app/src/main/java/com/example/roadcode/navitagion/MainNavGraph.kt
+++ app/src/main/java/com/example/roadcode/navitagion/MainNavGraph.kt
@@ -16,6 +16,7 @@ import com.example.roadcode.view.LevelTestScreen
 import com.example.roadcode.view.MypageScreen
 import com.example.roadcode.view.PasswordScreen
 import com.example.roadcode.view.PointScreen
+import com.example.roadcode.view.ProblemScreen
 import com.example.roadcode.view.RankingScreen
 import com.example.roadcode.view.RoadmapListScreen
 import com.example.roadcode.view.RoadmapPlanAlgorithmScreen
@@ -28,6 +29,7 @@ import com.example.roadcode.viewmodel.CalendarViewModel
 import com.example.roadcode.viewmodel.LevelTestViewModel
 import com.example.roadcode.viewmodel.PasswordViewModel
 import com.example.roadcode.viewmodel.PointViewModel
+import com.example.roadcode.viewmodel.ProblemViewModel
 import com.example.roadcode.viewmodel.RankingViewModel
 import com.example.roadcode.viewmodel.RoadmapPlanViewModel
 import com.example.roadcode.viewmodel.RoadmapViewModel
@@ -39,6 +41,7 @@ fun MainNavGraph(navController: NavHostController = rememberNavController()) {
     val roadmapPlanViewModel: RoadmapPlanViewModel = hiltViewModel()
     val levelTestViewModel: LevelTestViewModel = hiltViewModel()
     val roadmapViewModel: RoadmapViewModel = hiltViewModel()
+    val problemViewModel: ProblemViewModel = hiltViewModel()
     val rankingViewModel: RankingViewModel = hiltViewModel()
     val calendarViewModel: CalendarViewModel = hiltViewModel()
     val pointViewModel: PointViewModel = hiltViewModel()
@@ -52,7 +55,8 @@ fun MainNavGraph(navController: NavHostController = rememberNavController()) {
         composable("level_ready") { LevelTestReadyScreen(navController, roadmapPlanViewModel, levelTestViewModel) }                     // 레벨 테스트 준비 화면
         composable("level_test") { LevelTestScreen(navController, roadmapPlanViewModel, levelTestViewModel) }                           // 레벨 테스트 화면
         composable("level_result") { LevelTestResultScreen(navController, roadmapPlanViewModel, levelTestViewModel, roadmapViewModel) } // 레벨 테스트 결과 화면
-        composable("roadmap") { RoadmapScreen(navController, roadmapViewModel) }                                                        // 로드맵 조회 화면
+        composable("roadmap") { RoadmapScreen(navController, roadmapViewModel, problemViewModel) }                                                        // 로드맵 조회 화면
+        composable("problem") { ProblemScreen(navController, problemViewModel, roadmapViewModel) }                                                                          // 문제 풀이 화면
         composable("mypage") { MypageScreen(navController) }                                                                            // 마이페이지 화면
         composable("point") { PointScreen(navController, pointViewModel) }                                                              // 포인트 내역 조회 화면
         composable("userInfo") { UserInfoScreen(navController, userViewModel) }                                                         // 회원 정보 조회 화면
